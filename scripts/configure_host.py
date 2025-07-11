07ec53ed43208e8061542a119136100c07555f9e
Nicolai <nije@pch.dk>
2025-07-11 13:02:45 +0200

    link update

---
+++ scripts/configure_host.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+
+# Add the parent directory to sys.path so 'lib' can be imported
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from lib import common
